0ff88ca45ea4603d0af474fc7bbf471b3a7146da
correaa1 <bruno_correa_ds@hotmail.com>
2022-10-27 21:52:15 -0300

    google maps, primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const [state, setState] = useState({
+    latitude: -27.5579069,
+    longitude: -48.6281753,
+    latitudeDelta:0.0922,
+    longitudeDelta:0.0421, })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      style={styles.mapa}
+       setState {...state}
+      />
     </View>
   );
 }
@@ -17,4 +26,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapa:{
+    width: "100%",
+    height:750,
+  }
 });
